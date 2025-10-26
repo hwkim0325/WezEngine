6e9b5cf8115f09636f57833517c9cff077eb2fbe
Hyunwoo Kim <artprogrammer@naver.com>
2025-10-26 14:43:26 +0900

    Logクラス追加

---
+++ Wez/src/Wez/Log.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <memory>
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+namespace Wez {
+
+	class WEZ_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCorelogger() { return coreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClinetLogger() { return clientLogger; }
+
+	private:
+		static std::shared_ptr<spdlog::logger> coreLogger;
+		static std::shared_ptr<spdlog::logger> clientLogger;
+	};
+
+}
+
+// Core log macros
+#define WEZ_CORE_ERROR(...)		::Wez::Log::GetCorelogger()->trace(__VA_ARGS__)
+#define WEZ_CORE_INFO(...)		::Wez::Log::GetCorelogger()->info(__VA_ARGS__)
+#define WEZ_CORE_WARN(...)		::Wez::Log::GetCorelogger()->warn(__VA_ARGS__)
+#define WEZ_CORE_ERROR(...)		::Wez::Log::GetCorelogger()->error(__VA_ARGS__)
+#define WEZ_CORE_FATAL(...)		::Wez::Log::GetCorelogger()->fatal(__VA_ARGS__)
+
+
+// Client log macros
+#define WEZ_CLIENT_ERROR(...)	::Wez::Log::GetClinetLogger()->trace(__VA_ARGS__)
+#define WEZ_CLIENT_INFO(...)	::Wez::Log::GetClinetLogger()->info(__VA_ARGS__)
+#define WEZ_CLIENT_WARN(...)	::Wez::Log::GetClinetLogger()->warn(__VA_ARGS__)
+#define WEZ_CLIENT_ERROR(...)	::Wez::Log::GetClinetLogger()->error(__VA_ARGS__)
+#define WEZ_CLIENT_FATAL(...)	::Wez::Log::GetClinetLogger()->fatal(__VA_ARGS__)
+
